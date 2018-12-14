91e2328ce1fb96296aa42e95c1d9085b6ec535a0
Brad Dausses <brad.dausses@gmail.com>
2018-12-14 10:00:02 -0600

    Working with remote_file
    Signed-off-by: Brad Dausses <brad.dausses@gmail.com>

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,40 @@
 # Recipe:: default
 #
 # Copyright:: 2018, Chef Software, All Rights Reserved.
+
+# Create /opt/cylance
+directory '/opt/cylance' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  recursive true
+  action :create
+end
+
+# Configuring config_defaults.txt
+template '/opt/cylance/config_defaults.txt' do
+  source 'cylance_config_defaults.txt.erb'
+  owner 'root'
+  group 'root'
+  mode '0644'
+  action :create
+end
+
+remote_file '/tmp/CylancePROTECT.el7.rpm' do
+  source 'https://s3.amazonaws.com/sysco-security/cylance/CylancePROTECT.el7.rpm'
+  owner 'root'
+  group 'root'
+  mode '0644'
+  action :create
+end
+
+# Install Cylance
+package 'CylancePROTECT' do
+  source '/tmp/CylancePROTECT.el7.rpm'
+  action :install
+end
+
+# Start and enable Cylance
+service 'cylancesvc' do
+  action [:enable, :start]
+end
